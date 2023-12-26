2c19a76cb14656c979a99640143c0049193f15b6
KRISHNAKUMAR <gmkrishnakumar333.gmail.com>
2023-12-26 11:06:57 +0530

    Webpage with only html structure

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+
+button = document.querySelector('button');
+
+button.addEventListener('click',()=>{
+    first_password = document.querySelector('#password').value;
+    second_password = document.querySelector("#confirm-password").value;
+    email = document.querySelector('#email')
+
+    
+    if email.value.includes('@') and email.value.includes('.com') ? on  
+    
+    if (first_password != second_password){
+        console.log("Make sure that last password should match with previously entered")
+    }
+    
+
+})
